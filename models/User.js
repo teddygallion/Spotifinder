3568fb1c1427287259e7f43c0700cad20fb93edb
Teddy <ted.j.gallion@gmail.com>
2025-03-21 01:43:08 -0500

    wip: creates User model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  spotifyId: { type: String, unique: true, sparse: true }, // Optional Spotify linkage
+  playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }], // Reference to user's playlists
+  createdAt: { type: Date, default: Date.now },
+  library:[{
+    spotifyId: { type: String, required: true },
+    title: { type: String, required: true },
+    duration_ms: { type: Number },
+    artist: { type: String, required: true },
+    album: { type: String, required: true },
+    albumArt: { type: String, required: true }
+  }]
+});
+
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+module.exports = User;
